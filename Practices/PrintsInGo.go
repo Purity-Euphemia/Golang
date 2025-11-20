e66d9b26c7f4f0aa5cb3ea4d11203c8f2107cdb9
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-20 22:58:22 +0100

    my first commit

--- Practices/PrintsInGo.go
+++ Practices/PrintsInGo.go
@@ -12,9 +12,13 @@ func main() {
 	fmt.Println("world \n")
 	fmt.Println("goodbye everyone \n")
 
-
+	//println
 	fmt.Println("hello everyone")
 	fmt.Println("goodbye everyone")
 	fmt.Println("my age is", age, "and my name is", name)
+
+	
+	// printf(formatted string)
+	fmt.Printf("my age is %v and my name is %v", age, name)
 
 }
