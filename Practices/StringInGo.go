dff4b50ccd892f6996c15b1144fdc5245ca54242
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-20 22:20:15 +0100

    my first commit

--- Practices/StringInGo.go
+++ Practices/StringInGo.go
@@ -7,7 +7,14 @@ func main() {
 var nameOne string = "amaera"
 var nameTwo = "juligi"
 var nameThree string
-
 fmt.Println(nameOne, nameTwo, nameThree)
+
+
+nameOne = "peach"
+nameThree = "bowster"
+fmt.Println(nameOne, nameTwo, nameThree)
+
+nameFour := "yoshi" 
+fmt.Println(nameFour)
 
 }
