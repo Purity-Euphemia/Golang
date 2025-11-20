19f544481a544103050badf99e67dd749f7852e9
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-20 22:54:16 +0100

    my first commit

---
+++ Practices/PrintsInGo.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 35
+	name := "love"
+	
+	//print
+
+	fmt.Println("hello")
+	fmt.Println("world \n")
+	fmt.Println("goodbye everyone \n")
+
+
+	fmt.Println("hello everyone")
+	fmt.Println("goodbye everyone")
+	fmt.Println("my age is", age, "and my name is", name)
+
+}
