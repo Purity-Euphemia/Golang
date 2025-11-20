d86f382275cae2cad1d2d0fd62efe926edc76420
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-20 22:35:04 +0100

    my first commit

---
+++ Practices/intInGo.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+var ageOne int = 20
+var ageTwo int = 30
+ageThree := 40
+
+fmt.Println(ageOne, ageTwo, ageThree)
+
+
+//var numOne int8 = 25
+//var numTwo int8 = -129
+//var numThree unit8 = 256 
+
+//fmt.Println(numOne, numTwo, numThree)
+
+}
