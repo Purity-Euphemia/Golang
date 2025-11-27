c2acb7718cc2dad7bbc9d61b288f05f1bc5a8bae
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-11-27 08:17:18 +0100

    my first commit

---
+++ Practices/AddFront.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func AddFront(s string, slice []string) []string {
+	if s == "" {
+		return slice
+
+}
+	return append([]string{s}, slice...)
+	
+}
+
+
+func main() {
+    fmt.Println(AddFront("Hello", []string{"world"}))
+    fmt.Println(AddFront("Hello", []string{"world", "!"}))
+    fmt.Println(AddFront("Hello", []string{}))
+}
