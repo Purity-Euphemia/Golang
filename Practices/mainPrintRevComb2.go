3502bb7a874e907f9fde7a7ca25eef779d618159
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-12-09 22:32:45 +0100

    my first commit

---
+++ Practices/mainPrintRevComb2.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	first := true
+
+	for a := 9; a >= 0; a-- {
+		for b := 9; b >= 0; b-- {
+			for c := 9; c >= 0; c-- {
+
+				if a > b && b > c {
+					if !first {
+						fmt.Print(", ")
+					}
+					fmt.Printf("%d%d%d", a, b, c)
+					first = false
+				}
+			}
+		}
+	}
+
+	fmt.Println()
+}
