46989aea171bf5050d32452ae1ab91c48942cb72
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-01-12 03:54:07 +0100

    my work

---
+++ Practices/countLetters.go
@@ -0,0 +1,9 @@
+func countLetters(s string) int {
+    count := 0
+    for _, ch := range s {
+        if (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') {
+            count++
+        }
+    }
+    return count
+}
