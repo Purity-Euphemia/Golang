e25557d9c15ad7adc92262190a3b18ea43fedc69
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-01-25 03:53:50 +0100

    my work

---
+++ Practices/filterEven.go
@@ -0,0 +1,9 @@
+func filterEven(nums []int) []int {
+    result := []int{}
+    for _, n := range nums {
+        if n%2 == 0 {
+            result = append(result, n)
+        }
+    }
+    return result
+}
