8d129a328e6b9621a227b31a2999937ec508b0e4
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-01-26 21:46:46 +0100

    my work

---
+++ Practices/isPalindrome2.go
@@ -0,0 +1,9 @@
+func isPalindrome(s string) bool {
+    r := []rune(s)
+    for i := 0; i < len(r)/2; i++ {
+        if r[i] != r[len(r)-1-i] {
+            return false
+        }
+    }
+    return true
+}
